e19fff43d42935b3abe07ac563db970f188dcd8f
Dakota <drakoden2000@gmail.com>
2018-02-12 19:57:29 -0800

    working state

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,37 @@
 var compChoice = Math.floor(Math.random() * 102) + 19
-
-<<<<<<< HEAD
-console.log(compChoice);
-
-var points = 0
-
-
-function crystalValue() {
-    var userChoice = Math.floor(Math.random() * 12) + 1
-    console.log(userChoice);
-}
-
-$(".crystal-image").on("click", function() {
-    console.log("you clicked a crystal!");
-    crystalValue();
-    points++;
-})
-=======
 console.log(compChoice)
-
 var points = 0;
 var wins = 0;
 var losses = 0;
-document.getElementById("hitscore").innerHTML = compChoice;
-document.getElementById("wincount").innerHTML = wins;
-document.getElementById("losscount").innerHTML = losses;
+function winsIncrease() {
+    wins++;
+    document.getElementById("wincount").innerHTML = wins;
+}
+
+function lossIncrease() {
+    losses++;
+    document.getElementById("losscount").innerHTML = losses;
+}
+function crystalClick() {
+    points += Math.floor(Math.random() * 12) + 1;
+    console.log(points)
+}
+
 function reset() {
     points = 0;
-    var compChoice = Math.floor(Math.random() * 102) + 19
+    compChoice = Math.floor(Math.random() * 102) + 19;
 }
 $(".crystal-image").on("click", function () {
-    console.log("crystal clicked");
-    points += Math.floor(Math.random() * 12) + 1
+    crystalClick();
     document.getElementById("myscore").innerHTML = points;
-    console.log(points);
+    document.getElementById("hitscore").innerHTML = compChoice;
     if (points === compChoice) {
-        wins++;
+        winsIncrease();
         reset();
     }
     else if (points > compChoice) {
-        losses++;
+        lossIncrease();
         reset();
     }
 })
 
->>>>>>> origin/master
